dbaf12d0184bbf51bb35b9b638588c826dc4112e
gary <gary.harpaz@gmail.com>
2015-10-06 02:08:59 +0300

    CLI set up

---
+++ CLI/src/main/java/org/ppojo/Main.java
@@ -0,0 +1,12 @@
+package org.ppojo;
+
+/**
+ * Created by GARY on 10/6/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellow world6");
+        ArtifactOptions artifactOptions=new ArtifactOptions("abc",null,ArtifactOptions.getDefaultOptions());
+
+    }
+}
